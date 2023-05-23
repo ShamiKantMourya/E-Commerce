93d58c78ad3ade7197e9258386331f13e8ab578e
Shami_Mourya <shamimourya9852@gmail.com>
2023-05-23 12:24:20 +0530

    image js file

---
+++ src/frontend/image/index.js
@@ -0,0 +1,14 @@
+const IMAGES = {
+ bannerImg: require('./pet banner1.jpg'),
+ petGroom: require('./dogs grooming.png'),
+ petTreat: require('./dogs treat.png'),
+ logo: require('./logo.jpg'),
+ petToy: require('./pet toys.png'),
+ drool: require('./drools.jpeg'),
+ petFood: require('./dogs food.png'),
+ petCare: require('./pet care.png'),
+ Avatar: require('./avatar.png'),
+ 
+}
+
+export default IMAGES;
