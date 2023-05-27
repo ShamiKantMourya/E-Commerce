3faf4e594bf0f53c6d8830e5380a78474a5195ef
Shami_Mourya <shamimourya9852@gmail.com>
2023-05-27 22:45:52 +0530

    update in image props

---
+++ src/backend/db/productImage/index.js
@@ -0,0 +1,12 @@
+const IMAGES = {
+    Dog: require('./dogs.jpg'),
+    Cat: require('./cats.jpg'),
+    Bird: require('./birds.jpg'),
+    Rodents: require('./rodents.jpg'),
+    petToy: require('./pet toys.png'),
+    petFood: require('./dogs food.png'),
+    petCare: require('./pet care.png'),
+    petShelter: require('./petShelter.jpg')
+}
+
+export default IMAGES;
