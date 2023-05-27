200bca16598dbc2f24ea724ff04fc660b0764943
Shami_Mourya <shamimourya9852@gmail.com>
2023-05-27 22:41:35 +0530

    update in login handler

--- src/frontend/Contexts/AuthContext.js
+++ src/frontend/Contexts/AuthContext.js
@@ -1,58 +1,86 @@
-import { createContext, useState} from "react";
+import { createContext, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
-export function AuthProvider({children}) {
-  const [userEmail,setUserEmail]=useState("");
-  const  [userPassword, setUserPassword]=useState("");
-  const [signUpDetails , setSignUpDetails] = useState({
-    firstname: "",
-    lastname: "",
+export function AuthProvider({ children }) {
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [signUpDetails, setSignUpDetails] = useState({
+    fullName: "",
+    userName: "",
     emailId: "",
-    password:"",
-    confirmPassword:""
+    phoneNumber: "",
+    password: "",
+    confirmPassword: ""
   });
- 
+
   const location = useLocation();
   const navigate = useNavigate();
 
-  const getLoginToken=async ()=>{
-    const creds ={
-        email: userEmail,
-        password:userPassword
+  const getLoginToken = async () => {
+    const creds = {
+      email: userEmail,
+      password: userPassword
     }
-    try{
-        const response= await fetch('/api/auth/login',{
-        method:"POST",
-        body:JSON.stringify(creds)
-     }
-     )
-     const {encodedToken}= await response.json();
-     localStorage.setItem("encodedLoginToken",encodedToken);
-     console.log(encodedToken)
-    }catch(error){
-        console.log(error);
-    }
-}
-
-const encodedLoginToken = localStorage.getItem("encodedUserLoginToken");
-
-const handleLogin = async () => {
-  if (!encodedLoginToken) {
-    await getLoginToken();
-    if (encodedLoginToken !== undefined) {
-      if (location?.state?.from === undefined) {
-        navigate("/");
-      } else {
-        navigate(location?.state?.from);
+    try {
+      const response = await fetch('/api/auth/login', {
+        method: "POST",
+        body: JSON.stringify(creds)
       }
+      )
+
+      const { encodedToken, foundUser } = await response.json();
+      localStorage.setItem("encodedLoginToken", encodedToken);
+      localStorage.setItem("userLoginDetails", JSON.stringify(foundUser));
+        if (location?.state?.from === undefined) {
+          navigate("/");
+        } else {
+          navigate(location?.state?.from);
+        }
+      console.log(encodedToken)
+    } catch (error) {
+      console.log(error);
     }
   }
-}
+
+  const encodedLoginToken = localStorage.getItem("encodedLoginToken");
+  const userLoginDetails = localStorage.getItem("userLoginDetails");
+
+  const handleLogin = () => {
+    getLoginToken();
+  };
+
+  //SignUp
+
+  const getSignUpToken = async () => {
+    try {
+      const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        body: JSON.stringify(signUpDetails),
+      });
+      console.log(await response.json());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // const encodedSignUpToken = localStorage.getItem("encodedSignUpToken");
+
+  const signUpHandler = async () => {
+    await getSignUpToken();
+  }
+
+  //LogOut
+
+  const logoutHandler = () => {
+    localStorage.removeItem("encodedLoginToken");
+    navigate("/");
+  };
+
   return (
-     <AuthContext.Provider value={{handleLogin,userEmail,setUserEmail,userPassword,setUserPassword,signUpDetails,setSignUpDetails}}>
-            {children}
-      </AuthContext.Provider>
+    <AuthContext.Provider value={{ handleLogin, userEmail, setUserEmail, userPassword, setUserPassword, signUpDetails, setSignUpDetails, signUpHandler, userLoginDetails, logoutHandler }}>
+      {children}
+    </AuthContext.Provider>
   )
 }
