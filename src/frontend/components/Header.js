17094f96083ac64c93251357bb73039065190d50
Shami_Mourya <shamimourya9852@gmail.com>
2023-05-27 19:53:41 +0530

    header ui updated

--- src/frontend/components/Header.js
+++ src/frontend/components/Header.js
@@ -1,41 +1,74 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone,faEnvelope,faMagnifyingGlass,faCartShopping,faHeart} from '@fortawesome/free-solid-svg-icons';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhone,
+  faEnvelope,
+  faMagnifyingGlass,
+  faCartShopping,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
 import "./CSS/header.css";
 
-import {AuthContext} from "../Contexts/AuthContext";
+import { AuthContext } from "../Contexts/AuthContext";
 import IMAGES from "../image";
 
 function Header() {
   const encodedLoginToken = localStorage.getItem("encodedLoginToken");
-  const {logoutHandler} = useContext(AuthContext);
+  const { logoutHandler } = useContext(AuthContext);
   return (
-    <div>
+    <div className="headerParent">
       <div className="header">
-                    <h5 className="mob">{<FontAwesomeIcon icon={faPhone} />} +91-765477470*</h5>
-                    <p className="email"><FontAwesomeIcon icon={faEnvelope} /> shamikant300@gmail.com</p>
-                    <h4 className="code">We are Barking Discounts! Use Code HAPPYDOGO</h4>
-            </div>
-            <div className="logoPart">
-               <Link to= "/"><img className="logoImg" src= {IMAGES.logo} alt=" site logo" /></Link>
-               <form action="" className="searchBar">
-               <input
-                    type="text"
-                     id="header-search"
-                      placeholder= "search for your pet"
-                      name="s"
-                />
-                     <button type="submit">{<FontAwesomeIcon className="searchIcon" icon={faMagnifyingGlass} />}</button>
-               </form>
-               <div className="link">
-               <Link to= "/cart">{<FontAwesomeIcon className="cart" icon={faCartShopping} />}</Link>
-               <Link  to= "/wishlist">{<FontAwesomeIcon className="wishlist" icon={faHeart} />}</Link>
-              {(!encodedLoginToken) ? <Link to="/signin"> <button>Login</button></Link> :  <button onClick={logoutHandler}>Logout</button>}
-               </div>
-            </div>
+        {}
+        <p className="code">We are Barking Discounts! Use Code <strong>HAPPYDOGO</strong></p>
+        <div className="contact">
+          <p className="mob">
+            <span>{<FontAwesomeIcon icon={faPhone} />} </span>+91-765477470*
+          </p>
+          <p className="email">
+            <span><FontAwesomeIcon icon={faEnvelope} /> </span> shamikant300@gmail.com
+          </p>
+        </div>
+      </div>
+      <div className="logoPart">
+        <Link className="imgParent" to="/">
+          <img className="logoImg" src={IMAGES.logo} alt=" site logo" />
+        </Link>
+        <form action="" className="searchBar">
+          <input
+            type="text"
+            id="header-search"
+            placeholder="search for your pet"
+            name="s"
+          />
+          <button type="submit">
+            {
+              <FontAwesomeIcon
+                className="searchIcon"
+                icon={faMagnifyingGlass}
+              />
+            }
+          </button>
+        </form>
+        <div className="link">
+          <div className="linkChild"><Link to="/cart">
+            {<FontAwesomeIcon className="cart" icon={faCartShopping} />}
+          </Link>
+          <Link to="/wishlist">
+            {<FontAwesomeIcon className="wishlist" icon={faHeart} />}
+          </Link>
+          {!encodedLoginToken ? (
+            <Link to="/signin">
+              {" "}
+              <button>Login</button>
+            </Link>
+          ) : (
+            <button onClick={logoutHandler}>Logout</button>
+          )}</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header;
