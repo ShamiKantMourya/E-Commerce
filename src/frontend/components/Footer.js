1c62c47480bc0f8bba72907391a9e3cebcea5a83
Shami_Mourya <shamimourya9852@gmail.com>
2023-05-27 19:53:01 +0530

    footer ui updated

--- src/frontend/components/Footer.js
+++ src/frontend/components/Footer.js
@@ -1,40 +1,41 @@
 import React from 'react';
 import "../components/CSS/footer.css";
 import { Link } from 'react-router-dom';
+import { BsFacebook,BsInstagram,BsTwitter,BsYoutube } from 'react-icons/bs';
 
 
 const Footer = () => {
   return (
     <footer>
       <div className='footer-container'>
-        <div className='explore'>
+        <div className='explore box'>
             <h2>Explore</h2>
             <div className='links'>
-                <Link className='underline-hover-effect' to= "">Check Breeds</Link>
+                <Link className='underline-hover-effect' to= "">Our pets</Link>
                 <Link className='underline-hover-effect' to= "">Our Products</Link>
                 <Link className='underline-hover-effect' to= "">Curling Tails Blogs</Link>
             </div>
         </div>
-            <div className='needHelp'>
+            <div className='needHelp box'>
                 <h2>Need Help ?</h2>
-                <div className='links'>
+                <div className='links '>
                     <Link className='underline-hover-effect' to="">Track Your Order</Link>
                     <Link className='underline-hover-effect' to= "">Privacy Policy</Link>
                     <Link className='underline-hover-effect' to="">Return and Refunds</Link>
                 </div>
             </div>
-            <div className='stayConnected'>
+            <div className='stayConnected box'>
                 <h2>Stay Connected</h2>
                   <ul className='social-icons'>
-                    <li><i class="fa-brands fa-facebook"></i></li>
-                    <li><i class="fa-brands fa-instagram"></i></li>
-                    <li><i class="fa-brands fa-twitter"></i></li>
-                    <li><i class="fa-brands fa-youtube"></i></li>
+                    <li><i><BsFacebook /></i></li>
+                    <li><i><BsInstagram /></i></li>
+                    <li><i><BsTwitter /></i></li>
+                    <li><i><BsYoutube /></i></li>
                 </ul>
             </div>
       </div>
     <div class="wrapper">
-      <small>&copy;2023 <strong>Curling Tails</strong>, All Rights Reserved </small>
+      <h4>&copy;2023 <strong> Curling Tails</strong>, All Rights Reserved </h4>
       <nav class="footer-nav">
         <Link className='underline-hover-effect'  to= "">Back to Top</Link>
         <Link className='underline-hover-effect' to="">Terms of Use</Link>
