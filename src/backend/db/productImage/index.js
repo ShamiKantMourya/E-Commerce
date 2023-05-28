0712455c84ddf02dd647b66a30d3b803830bd1a2
Shami_Mourya <shamimourya9852@gmail.com>
2023-05-28 15:21:49 +0530

    product image update

--- src/backend/db/productImage/index.js
+++ src/backend/db/productImage/index.js
@@ -17,6 +17,21 @@ export const DogBreeds = {
 
 };
 export const catBreeds = {
-    
+    PersianCat: require('./Persian cat.jpg'),
+    BengalCat : require('./Bengal cat.jpg'),
+    RagdollCat: require('./Ragdoll cat.jpg'),
+    SiameseCat: require('./Siamese cat.jpg'),
+    HimalayanCat: require('./Himalayan cat.jpg'),
 };
+
+export const birdBreeds = {
+    Macaw: require('./macaw.jpg'),
+    Budgerigar: require('./Budgerigar.jpg')
+};
+
+export const rodentBreeds = {
+    Hamster: require('./Hamster.jpg'),
+    GuineaPig: require('./Guinea Pig.jpg'),
+};
+
 export default IMAGES ;
