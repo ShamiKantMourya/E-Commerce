92123aa38e4dd728d7e75a765685985ba1f2f13d
Shami_Mourya <shamimourya9852@gmail.com>
2023-05-28 15:22:42 +0530

    form ui update

--- src/frontend/authPage/signUp.js
+++ src/frontend/authPage/signUp.js
@@ -1,41 +1,54 @@
-import React from 'react'
+import React, { useContext } from 'react';
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "./signUp.css";
+import { AuthContext } from '../Contexts/AuthContext';
 
 const SignUp = () => {
+    const {signUpDetails,setSignUpDetails,signUpHandler} = useContext(AuthContext);
   return (
+    <>
+    <Header />
+    <div className='body'>
     <div className='signUp-container'>
         <div className='title'>Registration</div>
-        <form action='#'>
+        <form>
             <div className='user-details'>
                 <div className='input-box'>
                     <span className='details'>Full Name</span>
-                    <input type='text' placeholder='Enter your name' required />
+                    <input id='firstName' value={signUpDetails.fullName} onChange={(event) => setSignUpDetails({...signUpDetails , fullName: event.target.value
+                    })} type='text' placeholder='Enter your name' required />
                 </div>
                 <div className='input-box'>
                     <span className='details'>Username</span>
-                    <input type='text' placeholder='Enter your Username' required />
+                    <input id='lastName' value={signUpDetails.userName} onChange={(event) => setSignUpDetails({...signUpDetails, userName: event.target.value})} type='text' placeholder='Enter your Username' required />
                 </div>
                 <div className='input-box'>
                     <span className='details'>Email</span>
-                    <input type='text' placeholder='Enter your email' required />
+                    <input id='email' value={signUpDetails.emailId} onChange={(event) => setSignUpDetails({...signUpDetails, emailId: event.target.value})} type='text' placeholder='Enter your email' required />
                 </div>
                 <div className='input-box'>
                     <span className='details'>Phone Number</span>
-                    <input type='text' placeholder='Enter your phone number' required />
+                    <input id='phoneNumber' value={signUpDetails.phoneNumber} onChange={(event) => setSignUpDetails({...signUpDetails, phoneNumber: event.target.value})} type='text' placeholder='Enter your phone number' required />
                 </div>
                 <div className='input-box'>
                     <span className='details'>Password</span>
-                    <input type='text' placeholder='Enter your password' required />
+                    <input id='password' value={signUpDetails.password} onChange={(event) => setSignUpDetails({...setSignUpDetails, password: event.target.value})} type='password' placeholder='Enter your password' required />
                 </div>
                 <div className='input-box'>
                     <span className='details'>Confirm Password</span>
-                    <input type='text' placeholder='Re-enter your password' required />
+                    <input id='confirmPassword' value={signUpDetails.confirmPassword} onChange={(event)=> setSignUpDetails({...signUpDetails, confirmPassword: event.target.value})} type='password' placeholder='Re-enter your password' required />
                 </div>
             </div>
             <div className='registerbtn'>
-                <input type='submit' value="Register" />
+                <input type='submit' onClick={signUpHandler} value="Register" />
             </div>
         </form>
     </div>
+    </div>
+    <Footer />
+    </>
   )
 }
 
