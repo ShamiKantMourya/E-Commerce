98f51c18999b320a18931bc03ae664932667bfba
Shami_Mourya <shamimourya9852@gmail.com>
2023-05-29 10:20:52 +0530

    frontend image props update

--- src/frontend/image/index.js
+++ src/frontend/image/index.js
@@ -1,12 +1,10 @@
 const IMAGES = {
- bannerImg: require('./pet banner1.jpg'),
+ bannerImg: require('./bg.jpg'),
+ bannerBg: require('./petsbg.jpg'),
  petGroom: require('./dogs grooming.png'),
  petTreat: require('./dogs treat.png'),
- logo: require('./logo.jpg'),
- petToy: require('./pet toys.png'),
+ logo: require('./Asset 3.png'),
  drool: require('./drools.jpeg'),
- petFood: require('./dogs food.png'),
- petCare: require('./pet care.png'),
  Avatar: require('./avatar.png'),
  
 }
