f6b1df2cad46a4eaf02938b25570f9356bcf8c74
Shami_Mourya <shamimourya9852@gmail.com>
2023-05-29 10:18:33 +0530

    route update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Routes , Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 
 import { LandingPage } from "./frontend/landingPage/homePage"
@@ -9,29 +9,45 @@ import CartPage from "./frontend/cartPage/cartPage";
 import RequireAuth from "./frontend/components/RequireAuth";
 import Wishlist from "./frontend/wishlistPage/wishlist";
 import Dog from "./frontend/productPage/dog";
+import Cat from "./frontend/productPage/cat";
+import Bird from "./frontend/productPage/bird";
+import Rodent from './frontend/productPage/rodent';
+import Filter from "./frontend/components/filter";
 
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <>
-    <Routes>
-            <Route path = "/" element = {<LandingPage />} />
-            <Route path = "/mock-api" element = {<MockAPI />} />
-            <Route path="/signin" element = {<SignIn />} />
-            <Route path="/signup" element = {<SignUp />} />
-            <Route path="/cart" element = { 
-            // <RequireAuth>
-              <CartPage />
-            // </RequireAuth>
-          } />
-             <Route path="/wishlist" element = {
-              <RequireAuth>
-                <Wishlist />
-              </RequireAuth>
-            } />
-          {/* dog page route */}
-            <Route path="/dog" element = {<Dog />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/mock-api" element={<MockAPI />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/cart" element={
+          // <RequireAuth>
+            <CartPage />
+          // </RequireAuth>
+        } />
+        <Route path="/wishlist" element={
+          // <RequireAuth>
+            <Wishlist />
+          // </RequireAuth>
+        } />
+        {/* dog page route */}
+        <Route path="/dog" element={<Dog />} />
+
+        {/* cat page route */}
+        <Route path="/cat" element={<Cat />} />
+
+        {/* bird page route */}
+        <Route path="/bird" element = {<Bird />} />
+
+        {/* rodent page route */}
+        <Route path="/rodent" element = {<Rodent />}/>
+
+        <Route path="/filter" element = {<Filter />} />
+      </Routes>
+     
     </>
   )
 };
