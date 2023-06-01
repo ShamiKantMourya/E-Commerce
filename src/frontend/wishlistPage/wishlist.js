05e1eb5dfd52770956a191689f3c1aaf04c2fc87
Shami_Mourya <shamimourya9852@gmail.com>
2023-06-01 14:03:10 +0530

    update in wishlist card component name

--- src/frontend/wishlistPage/wishlist.js
+++ src/frontend/wishlistPage/wishlist.js
@@ -4,7 +4,7 @@ import "./wishlist.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import WishlistItemContext from '../Contexts/WishlistContext';
-import WIshlistCard from './WIshlistCard';
+import WishListCard from "./WishListCard";
 
 const Wishlist = () => {
   const {wishlistItem} = useContext(WishlistItemContext);
@@ -18,7 +18,7 @@ const Wishlist = () => {
         ): (
           wishlistItem.map((items)=>(
             <div className='wishlist-item' key={items._id}>
-              <WIshlistCard wishlistItem = {items} />
+            <WishListCard wishlistItem = {items}/>
             </div>
           ))
         )
