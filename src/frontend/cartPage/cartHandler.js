e77c0646b4160f0b22db7b56dac4c078ab9c88ca
Shami_Mourya <shamimourya9852@gmail.com>
2023-06-17 00:45:33 +0530

    cart handler

---
+++ src/frontend/cartPage/cartHandler.js
@@ -0,0 +1,66 @@
+import { toast } from "react-toastify";
+const encodedToken = localStorage.getItem("token");
+
+
+export const addToCartHandler = async (product, dispatch, navigate,
+  location) => {
+    if(encodedToken){
+  try {
+    console.log(product);
+    const response = await fetch("/api/user/cart", {
+      method: "POST",
+      body: JSON.stringify({ product }),
+      headers: {
+        authorization: encodedToken,
+      },
+    });
+    const cartData = await response.json();
+    console.log(cartData);
+    dispatch({ type: "add_to_cart", payLoad: cartData?.cart });
+    toast.success("Item added to Cart");
+  } catch (error) {
+    console.log(error);
+  }
+}else{
+  navigate("/login", { state: { from: location?.pathname } });
+}
+};
+export const removeFromCartHandler = async (productId, dispatch) => {
+
+  try {
+    const response = await fetch(`/api/user/cart/${productId}`, {
+      method: "DELETE",
+      headers: { authorization: encodedToken },
+    });
+
+    const cartData = await response.json();
+
+    dispatch({ type: "remove_from_cart", payLoad: cartData?.cart });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const cartQuantityHandler = async (productId, type, dispatch) => {
+  try {
+    console.log(productId, type);
+    const response = await fetch(`/api/user/cart/${productId}`, {
+      method: "POST",
+      body: JSON.stringify({
+        action: {
+          type: type,
+        },
+      }),
+      headers: { authorization: encodedToken },
+    }); 
+    const cartData = await response.json();
+
+    dispatch({ type: "update_cart_quantity", payLoad: cartData?.cart });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const itemIsInCart = (cart, productId) => {
+  return cart.some((product) => product._id === productId);
+};
