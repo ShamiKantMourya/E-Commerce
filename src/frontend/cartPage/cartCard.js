2586f1d7adf0726da87ec546660c309e1b6e4875
Shami_Mourya <shamimourya9852@gmail.com>
2023-06-17 00:44:48 +0530

    cartCard.js

---
+++ src/frontend/cartPage/cartCard.js
@@ -0,0 +1,84 @@
+import { useContext } from "react";
+import { ToastContainer } from "react-toastify";
+import { useLocation, useNavigate } from "react-router";
+import { FaStar } from "react-icons/fa"
+
+import "./cartCard.css"
+import {itemIsInWishlist} from "../wishlistPage/wishlistHandler";
+import { DataContext } from "../Contexts/dataContext";
+import {cartQuantityHandler, removeFromCartHandler} from "./cartHandler";
+import {addToWishlistHandler, removeFromWishlistHandler} from "../wishlistPage/wishlistHandler";
+
+export const CartCard = ({ product }) => {
+    const { _id, breed, price, image, qty, rating } =
+      product;
+  
+    // const encodedToken = localStorage.getItem("token");
+    const {wishlist, addDataDispatch } =
+      useContext(DataContext);
+  
+    const isProductInWishlist = itemIsInWishlist(wishlist, _id);
+    const navigate = useNavigate();
+    const location = useLocation();
+  
+    return (
+      <>
+        <ToastContainer />
+        <div className="cart-card-container">
+          <img src={image} alt={breed} />
+          <div className="cart-card-content">
+            <div className="cart-card-title">
+              <span>{breed}</span>
+              <span className="cart-card-rating">
+              <FaStar /> {rating} 
+              </span>
+            </div>
+            <span>{price}</span>
+            <div className="cart-card-quantity">
+              <button
+                onClick={() => cartQuantityHandler(_id, "decrement", addDataDispatch)}
+              >
+                -
+              </button>
+              <p>{qty}</p>
+              <button
+                onClick={() => cartQuantityHandler(_id, "increment", addDataDispatch)}
+              >
+                +
+              </button>
+            </div>
+            <div className="cart-card-buttons">
+              <button
+                className="cart-card-button"
+                onClick={() => removeFromCartHandler(_id, addDataDispatch)}
+              >
+                Remove
+              </button>
+              {isProductInWishlist ? (
+                <button
+                  className="cart-card-button"
+                  onClick={() => removeFromWishlistHandler(_id, addDataDispatch)}
+                >
+                  Remove from Wishlist
+                </button>
+              ) : (
+                <button
+                  className="cart-card-button"
+                  onClick={() =>
+                    addToWishlistHandler(
+                      product,
+                      addDataDispatch,
+                      navigate,
+                      location
+                    )
+                  }
+                >
+                  Add to Wishlist
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
