291f60ef0ec433731824b0efc2db54d6a51c081e
Shami_Mourya <shamimourya9852@gmail.com>
2023-06-30 14:08:24 +0530

    update in encoded name

--- src/frontend/components/RequireAuth.js
+++ src/frontend/components/RequireAuth.js
@@ -1,7 +1,7 @@
 import { Navigate , useLocation } from 'react-router-dom';
 
 function RequireAuth({children}) {
-  const encodedLoginToken = localStorage.getItem("encodedLoginToken");
+  const encodedLoginToken = localStorage.getItem("token");
   const location = useLocation();
   return encodedLoginToken ? (children) : ( <Navigate to="/signin" state={{ from: location }} />)
   
