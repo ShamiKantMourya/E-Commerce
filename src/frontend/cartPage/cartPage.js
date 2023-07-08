9f8b22bc2b4ac76e38cc951f2b4153432d65b57f
Shami_Mourya <88420924+ShamiKantMourya@users.noreply.github.com>
2023-07-09 00:00:56 +0530

    code refactor

--- src/frontend/cartPage/cartPage.js
+++ src/frontend/cartPage/cartPage.js
@@ -6,6 +6,7 @@ import { CartCard } from "./cartCard";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { CheckoutCard } from "../components/checkoutCard";
+import "./cartpage.css";
 
 const CartPage = () => {
   const { cart } = useContext(DataContext);
--- src/frontend/wishlistPage/wishlist.js
+++ src/frontend/wishlistPage/wishlist.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import {BsStar, BsFillBookmarkHeartFill} from "react-icons/bs";
+import { BsStar, BsFillBookmarkHeartFill } from "react-icons/bs";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
@@ -16,93 +16,99 @@ import "./wishlist.css";
 
 
 const Wishlist = () => {
-    const { cart, wishlist, addDataDispatch} = useContext(DataContext);
+    const { cart, wishlist, addDataDispatch } = useContext(DataContext);
 
     const navigate = useNavigate();
     const location = useLocation();
     console.log(wishlist);
-  return (
-    <div>
-        <Header/>
-        <Toaster position="bottom-right" reverseOrder={false} />
-        {
-           wishlist.map((product) => {
-            const   { _id, breed, image, price, rating } = product;
-            const isProductInCart = itemIsInCart(cart, _id) 
-            const isProductInWishlist = itemIsInWishlist(wishlist, _id);
-               return(
-               <div className='main-card'  key={_id}>
-                   <div className='breed-card'>
-                       <NavLink to={`/productdetails/${_id}`}>
-                           <div className='bgClickLayer'></div>
-                       </NavLink>
-                       {isProductInWishlist ? (
-                           <button
-                               style={{ color: "red" }}
-                               className="breed-wishlist"
-                               onClick={() =>
-                                   removeFromWishlistHandler(_id, addDataDispatch)
-                               }
-                           >
-                               <BsFillBookmarkHeartFill />
-                           </button>
-                       ) : (
-                           <button
-                               className="breed-wishlist"
-                               onClick={() =>
-                                   addToWishlistHandler(
-                                       product,
-                                       addDataDispatch,
-                                       navigate,
-                                       location
-                                   )
-                               }
-                           >
-                               <BsFillBookmarkHeartFill />
-                           </button>
-                       )}
-                       <div className='breed-image'>
+    return (
+        <div>
+            <Header />
+            <Toaster position="bottom-right" reverseOrder={false} />
+            {
+                wishlist.length === 0 ? (
+                    <div className="cart-header-text">
+                        <h1>Add something to wishlist</h1>
+                    </div>
+                ) :
+                    wishlist.map((product) => {
+                        const { _id, breed, image, price, rating } = product;
+                        const isProductInCart = itemIsInCart(cart, _id)
+                        const isProductInWishlist = itemIsInWishlist(wishlist, _id);
+                        return (
+                            <div className='main-card' key={_id}>
+                                <div className='breed-card'>
+                                    <NavLink to={`/productdetails/${_id}`}>
+                                        <div className='bgClickLayer'></div>
+                                    </NavLink>
+                                    {isProductInWishlist ? (
+                                        <button
+                                            style={{ color: "red" }}
+                                            className="breed-wishlist"
+                                            onClick={() =>
+                                                removeFromWishlistHandler(_id, addDataDispatch)
+                                            }
+                                        >
+                                            <BsFillBookmarkHeartFill />
+                                        </button>
+                                    ) : (
+                                        <button
+                                            className="breed-wishlist"
+                                            onClick={() =>
+                                                addToWishlistHandler(
+                                                    product,
+                                                    addDataDispatch,
+                                                    navigate,
+                                                    location
+                                                )
+                                            }
+                                        >
+                                            <BsFillBookmarkHeartFill />
+                                        </button>
+                                    )}
+                                    <div className='breed-image'>
 
-                           <img src={image} alt={breed} />
+                                        <img src={image} alt={breed} />
 
-                       </div>
+                                    </div>
 
-                       <p className='breed-name'>{breed}</p>
-                       <div className='price-rating'>
-                           <p className='breed-price'><strong>₹</strong> {price}</p>
-                           <p className='breed-rating'><BsStar className='star' />{rating}</p>
-                       </div>
-                       {isProductInCart ? (
-                           <button className="adoptbtn">
-                               <NavLink
-                                   to="/cart"
-                               >
-                                   Go to Cart
-                               </NavLink>
-                           </button>
-                       ) : (
-                           <button
-                               className="adoptbtn"
-                               onClick={() =>
-                                   addToCartHandler(
-                                       product,
-                                       addDataDispatch,
-                                       navigate,
-                                       location
-                                   )
-                               }
-                           >
-                               Add to cart
-                           </button>
-                       )}
-                   </div>
-               </div>
+                                    <p className='breed-name'>{breed}</p>
+                                    <div className='price-rating'>
+                                        <p className='breed-price'><strong>₹</strong> {price}</p>
+                                        <p className='breed-rating'><BsStar className='star' />{rating}</p>
+                                    </div>
+                                    {isProductInCart ? (
+                                        <button className="adoptbtn">
+                                            <NavLink
+                                                to="/cart"
+                                            >
+                                                Go to Cart
+                                            </NavLink>
+                                        </button>
+                                    ) : (
+                                        <button
+                                            className="adoptbtn"
+                                            onClick={() =>
+                                                addToCartHandler(
+                                                    product,
+                                                    addDataDispatch,
+                                                    navigate,
+                                                    location
+                                                )
+                                            }
+                                        >
+                                            Add to cart
+                                        </button>
+                                    )}
+                                </div>
+                            </div>
 
-           )})
-        }
-        <Footer/>
-    </div>
-  )
+                        )
+                    })
+            }
+            <Footer />
+        </div>
+    )
 }
 
 export default Wishlist;
