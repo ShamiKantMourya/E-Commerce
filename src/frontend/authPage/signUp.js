ad3818f11b523f0d7065c460f9882a6fa51cbbb6
Shami_Mourya <shamimourya9852@gmail.com>
2023-07-25 16:49:16 +0530

    register button

--- src/frontend/authPage/signUp.js
+++ src/frontend/authPage/signUp.js
@@ -7,8 +7,8 @@ import { AuthContext } from '../Contexts/AuthContext';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
-    const { signUpDetails, setSignUpDetails,signUpHandler } = useContext(AuthContext);
-    const {fullName,userName , email,password,phoneNumber,confirmPassword} = signUpDetails;
+    const { signUpDetails, setSignUpDetails, signUpHandler } = useContext(AuthContext);
+    const { firstName, lastName, email, password, phoneNumber, confirmPassword } = signUpDetails;
     return (
         <>
             <Header />
@@ -18,36 +18,46 @@ const SignUp = () => {
                     <form>
                         <div className='user-details'>
                             <div className='input-box'>
-                                <span className='details'>Full Name</span>
-                                <input id='firstName' value={fullName} onChange={(event) => setSignUpDetails({
-                                    ...signUpDetails, fullName: event.target.value
-                                })} type='text' placeholder='Enter your name' required />
+                                <span className='details'>Firstname</span>
+                                <input id='firstName' value={firstName} onChange={(event) => setSignUpDetails((prevState) => ({
+                                    ...prevState, firstName: event.target.value,
+                                }))} type='text' placeholder='Enter your name' required />
                             </div>
                             <div className='input-box'>
-                                <span className='details'>Username</span>
-                                <input id='lastName' value={userName} onChange={(event) => setSignUpDetails({ ...signUpDetails, userName: event.target.value })} type='text' placeholder='Enter your Username' required />
+                                <span className='details'>Lastname</span>
+                                <input id='lastName' value={lastName} onChange={(event) => setSignUpDetails((prevState) => ({
+                                    ...prevState, lastName: event.target.value,
+                                }))} type='text' placeholder='Enter your Username' required />
                             </div>
                             <div className='input-box'>
                                 <span className='details'>Email</span>
-                                <input id='email' value={email} onChange={(event) => setSignUpDetails({ ...signUpDetails, email: event.target.value })} type='text' placeholder='Enter your email' required />
+                                <input id='email' value={email} onChange={(event) => setSignUpDetails((prevState) => ({
+                                    ...prevState, email: event.target.value,
+                                }))} type='text' placeholder='Enter your email' required />
                             </div>
                             <div className='input-box'>
                                 <span className='details'>Phone Number</span>
-                                <input id='phoneNumber' value={phoneNumber} onChange={(event) => setSignUpDetails({ ...signUpDetails, phoneNumber: event.target.value })} type='text' placeholder='Enter your phone number' required />
+                                <input id='phoneNumber' value={phoneNumber} onChange={(event) => setSignUpDetails((prevState) => ({
+                                    ...prevState, phoneNumber: event.target.value,
+                                }))} type='text' placeholder='Enter your phone number' required />
                             </div>
                             <div className='input-box'>
                                 <span className='details'>Password</span>
-                                <input id='password' value={password} onChange={(event) => setSignUpDetails({ ...setSignUpDetails, password: event.target.value })} type='password' placeholder='Enter your password' required />
+                                <input id='password' value={password} onChange={(event) => setSignUpDetails((prevState) => ({
+                                    ...prevState, password: event.target.value,
+                                }))} type='password' placeholder='Enter your password' required />
                             </div>
                             <div className='input-box'>
                                 <span className='details'>Confirm Password</span>
-                                <input id='confirmPassword' value={confirmPassword} onChange={(event) => setSignUpDetails({ ...signUpDetails, confirmPassword: event.target.value })} type='password' placeholder='Re-enter your password' required />
+                                <input id='confirmPassword' value={confirmPassword} onChange={(event) => setSignUpDetails((prevState) => ({
+                                    ...prevState, confirmPassword: event.target.value,
+                                }))} type='password' placeholder='Re-enter your password' required />
                             </div>
                         </div>
                         <div className='registerbtn'>
-                            <input type='submit' onClick={signUpHandler} value="Register" />
+                            <button disabled = {password !== confirmPassword} type='submit' onClick={signUpHandler} className='register-btn'>Register</button>
                         </div>
-                        <Link to= "/login">Already have account<span
+                        <Link to="/login">Already have account<span
                         >Login</span></Link>
                     </form>
                 </div>
